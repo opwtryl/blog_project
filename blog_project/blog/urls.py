427b8c163acbfcf35186c30c6df99794ffdea1d6
zhanghonghua <457285383@qq.com>
2018-06-09 17:28:46 +0800

    侧边栏、分类&归档

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
 	path('',views.index,name='index'),
-	path('post/<int:pk>/',views.detail,name='detail')
+	path('post/<int:pk>/',views.detail,name='detail'),
+	path('archives/<int:year>/<int:month>/',views.archives,name='archives'),
+	path('category/<int:pk>/',views.category,name='category'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 import markdown
-from .models import Post
+from .models import Post, Category
 
 
 def index(request):
@@ -10,5 +10,18 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body,extensions=['markdown.extensions.extra','markdown.extensions.codehilite','markdown.extensions.toc',])
+    post.body = markdown.markdown(post.body, extensions=[
+                                  'markdown.extensions.extra', 'markdown.extensions.codehilite', 'markdown.extensions.toc', ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
