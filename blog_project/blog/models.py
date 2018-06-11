3569c11ffeb5d30a296a9f0178e322f01e466e7a
zhanghonghua <457285383@qq.com>
2018-06-11 14:46:45 +0800

    自动生成摘要

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-
+from django.utils.html import strip_tags
+import markdown
 # 分类
 
 
@@ -60,6 +61,13 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    def save(self,*args,**kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=['markdown.extensions.extra','markdown.extensions.codehilite',])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super(Post,self).save(*args,**kwargs)
 
     class Meta:
         ordering = ['-created_time']
