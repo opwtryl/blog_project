769cb3b3060641b19afa7615c8f5af4cf9b3eaf5
zhanghonghua <457285383@qq.com>
2018-06-12 10:16:13 +0800

    实现标签功能

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('post/<int:pk>/',views.PostDetailView.as_view(),name='detail'),
 	path('archives/<int:year>/<int:month>/',views.ArchivesView.as_view(),name='archives'),
 	path('category/<int:pk>/',views.CategoryView.as_view(),name='category'),
+	path('tag/<int:pk>/',views.TagView.as_view(),name='tag'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 import markdown
-from .models import Post, Category
+from .models import Post, Category,Tag
 from comments.forms import CommentForm
 from django.views.generic import ListView, DetailView
 
@@ -98,6 +98,12 @@ class CategoryView(IndexView):
     def get_queryset(self):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class TagView(IndexView):
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
 
 
 class ArchivesView(IndexView):
